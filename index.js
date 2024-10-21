015629e05ff51e271830b92f3fcbc78dd7a24b4a
MrB1390 <139531877+MrB1390@users.noreply.github.com>
2024-10-21 16:58:33 +0530

    updated the code as per postgres

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './Database/db.connect.js';
+import { connectDB } from './Database/db.connect.js';
 import router from './Router/router.gamer.js';
+import sequelize from './Database/db.connect.js';
 
 
 dotenv.config();
@@ -21,6 +22,13 @@ app.get('/',(req,res) =>{
 
 const port = process.env.PORT;
 
-app.listen(port,()=>{
+app.listen(port, async ()=>{
      console.log(`App is running on Port ${port}`);
+
+     try {
+        await sequelize.sync(); // This will create the tables if they don't exist
+        console.log('Database synchronized successfully');
+      } catch (error) {
+        console.error('Error syncing database:', error);
+      }
 })
